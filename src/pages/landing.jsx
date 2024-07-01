16d9fb23365c48dc2c18750365662ee203e09aad
AsmaaAbuubakar <145564381+AsmaaAbuubakar@users.noreply.github.com>
2024-07-01 02:39:26 -0700

    added landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,5 +1,51 @@
-function landing() {
-  return <h1>landing page</h1>;
+import Hero from "../Components/hero";
+import Pet from "../Components/pet";
+import SearchPet from "../Components/searchpet";
+import { PetsState } from "../Context/context";
+
+function Landing() {
+  const { petData, setPetData, tempPetData, isPending } = PetsState();
+
+  // searching for specific Pet by it's name
+  function searchPetName(name) {
+    let tempData = tempPetData;
+    if (name) {
+      tempData = tempData.filter((pet) => {
+        const smallName = pet.name.toLowerCase();
+        if (smallName.includes(name.toLowerCase())) {
+          return Pet;
+        }
+      });
+    } else {
+      tempData = tempPetData;
+    }
+    setPetData(tempData);
+  }
+
+  const allPets = petData.map((pet) => <Pet key={pet.id} petData={pet} />);
+  return (
+    <main className="w-full">
+      <Hero />
+
+      <section className="py-4">
+        <h1 className="font-bold text-3xl w-full text-center my-10 capitalize">
+          all Pets
+        </h1>
+        <SearchPet searchPetName={searchPetName} />
+        {isPending ? (
+          <section className=" w-full h-full flex justify-center items-center">
+            <div className=" w-24 h-24 border-4 border-blue-500 border-dotted rounded-full animate-spin flex justify-center items-center text-lg">
+              loading...
+            </div>
+          </section>
+        ) : (
+          <div className="container mx-auto flex flex-wrap items-center justify-center gap-7">
+            {allPets}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
 
-export default landing;
+export default Landing;
