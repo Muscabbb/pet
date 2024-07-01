739cc7a08a480e463cad324c49da53806276fdf0
AsmaaAbuubakar <145564381+AsmaaAbuubakar@users.noreply.github.com>
2024-07-01 02:50:47 -0700

    Update landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -32,17 +32,9 @@ function Landing() {
           all Pets
         </h1>
         <SearchPet searchPetName={searchPetName} />
-        {isPending ? (
-          <section className=" w-full h-full flex justify-center items-center">
-            <div className=" w-24 h-24 border-4 border-blue-500 border-dotted rounded-full animate-spin flex justify-center items-center text-lg">
-              loading...
-            </div>
-          </section>
-        ) : (
           <div className="container mx-auto flex flex-wrap items-center justify-center gap-7">
             {allPets}
           </div>
-        )}
       </section>
     </main>
   );
